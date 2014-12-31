3d0030468c4dbbfaf19ad366812a9d963fdf0052
Marco Secreto <msegreto@miceover.com>
2014-12-30 22:09:02 -0800

    Functionality to get background color of element
    Fixes #8

--- test/page/dom_test.js
+++ test/page/dom_test.js
@@ -4,9 +4,9 @@ var test = require('tapes');
 
 var dom = require('../../lib/page/dom');
 
-test('getColor', function(t) {
+test('getForeColor', function(t) {
   t.test('exists', function(t) {
-    t.ok(dom.getColor);
+    t.ok(dom.getForeColor);
     t.end();
   });
   t.test('returns css color as hex', function(t) {
@@ -18,7 +18,25 @@ test('getColor', function(t) {
       .css('color', expected)
       .text('testText');
 
-    actual = dom.getColor($testEl);
+    actual = dom.getForeColor($testEl);
+
+    t.equal(actual, expected, 'The color is the css color value as a hex');
+
+    t.end();
+  });
+  t.end();
+});
+test('getBackgroundColor', function(t) {
+  t.test('returns css color as hex', function(t) {
+    var expected = '#1188ff',
+        actual,
+        $testEl;
+
+    $testEl = $('<p>')
+      .css('backgroundColor', expected)
+      .text('testText');
+
+    actual = dom.getBackgroundColor($testEl);
 
     t.equal(actual, expected, 'The color is the css color value as a hex');
 
